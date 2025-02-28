2fdb0f5e547789219cc0ddf58d2060660585011f
Josef Alirani <joal2203@student.miun.se>
2025-02-28 15:59:17 +0100

    lagt till GUI formulär, README- & requirements- fil

---
+++ README.md
@@ -0,0 +1,8 @@
+# LLM job analyzer
+For MIUN course DT002G, VT2025
+
+## Setup
+
+```commandline
+    pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,17 +1,67 @@
-# This is a sample Python script.
+import sys
+
+from PyQt6.QtCore import QDate
+from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QDateEdit, QPushButton, QGridLayout, QLabel, QLayout, \
+    QFormLayout
 import ApiService
-# Press Skift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class Main:
+    def __init__(self):
+        app = QApplication([])
+        window = QWidget()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        window.setWindowTitle("LLM job analyzer")
+        window.setGeometry(100, 100, 400, 600)
 
+        layout = QFormLayout()
 
-# Press the green button in the gutter to run the script.
+        self.locationField = QLineEdit()
+        layout.addRow("location:", self.locationField)
+
+        self.useDate = QPushButton("Use date")
+        self.useDate.setCheckable(True)
+        self.useDate.clicked.connect(self.check_box)
+
+        self.startDate = QDateEdit(calendarPopup=True)
+        self.startDate.setMinimumDate(QDate(2016, 1,1))
+        self.startDate.setMaximumDate(QDate.currentDate())
+
+        self.endDate = QDateEdit(calendarPopup=True)
+        self.endDate.setMinimumDate(QDate(2016, 1, 1))
+        self.endDate.setMaximumDate(QDate.currentDate())
+        layout.addRow(self.useDate)
+        layout.addRow("start-date:", self.startDate)
+        layout.addRow("end-date:", self.endDate)
+
+        self.extraApis = QLineEdit()
+        layout.addRow(QLabel("additional API:s (separate with ','):"))
+        layout.addRow(self.extraApis)
+
+        self.submitButton = QPushButton("start")
+        self.submitButton.clicked.connect(self.test_button)
+        layout.addRow(self.submitButton)
+
+        window.setLayout(layout)
+        window.show()
+        sys.exit(app.exec())
+
+    def test_button(self):
+        print ("skickat")
+        self.submitButton.setEnabled(False)
+        # apiService = ApiService.ApiService()
+        # apiService.load()
+
+    def check_box(self):
+        if self.useDate.isChecked():
+            self.useDate.setCheckable(False)
+            self.startDate.setEnabled(True)
+            self.endDate.setEnabled(True)
+        else:
+            self.useDate.setCheckable(True)
+            self.startDate.setEnabled(False)
+            self.endDate.setEnabled(False)
+
 if __name__ == '__main__':
-    apiService = ApiService.ApiService()
-    apiService.load()
+    Main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
